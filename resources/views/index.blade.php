9e8ab019c61de04ff19a3b31ba95ce606890dc7b
FriendlyVideogamePlayer <zakariahahah@gmail.com>
2021-07-31 22:56:29 +0100

    Move head to separate blade file and make home page pretty

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,25 +1,19 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="UTF-8"/>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-        <title>Laravel</title>
-        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
-        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-    </head>
+    @include('components.head')
     <body class="bg-gradient-to-r from-indigo-500 via-pink-500 to-yellow-500">
-        <div class="container mx-auto bg-cover min-h-screen w-full flex justify-center items-center">
-            <div class="w-full min-h-screen my-4 bg-white p-5 rounded-xl bg-opacity-40 backdrop-filter backdrop-blur-lg">
+        <div class="w-full min-h-screen bg-white bg-opacity-20 backdrop-filter backdrop-blur-md">    
+            <div class="container mx-auto bg-cover min-h-screen w-full flex justify-center items-center">
                 <div class="header-card justify-center flex font-semibold">
                     <div class="text-center">
-                        <h1 class="text-xl pb-5 pt-5" role="heading" aria-level="1">ffxiv board </h1>
-                        <div class="index">
-                            <div class="mb-4">
-                                <a href="" class='justify-center flex w-44 bg-gradient-to-r from-indigo-500 via-pink-500 to-yellow-500 hover:from-indigo-600 hover:via-pink-600 hover:to-red-600 focus:outline-none text-white text-lg font-bold shadow-md rounded-full mx-auto p-2'>
-                                    <div class="flex">post an ad!</div>
-                                </a>
-                            </div>
-                            </p>
+                        <h1 class="text-5xl text-white pb-6 pt-5" role="heading" aria-level="1">ffxiv job board </h1>
+                        <div class="flex flex-row gap-x-3 mb-4">
+                            <a href="" class='justify-center flex w-40 bg-gradient-to-r from-indigo-500 via-pink-500 to-yellow-500 hover:from-indigo-600 hover:via-pink-600 hover:to-red-600 focus:outline-none text-white text-lg font-bold shadow-md rounded-full mx-auto p-2'>
+                                <div class="flex">post an ad!</div>
+                            </a>
+                            <a href="" class='justify-center flex w-40 bg-gradient-to-r from-indigo-500 via-pink-500 to-yellow-500 hover:from-indigo-600 hover:via-pink-600 hover:to-red-600 focus:outline-none text-white text-lg font-bold shadow-md rounded-full mx-auto p-2'>
+                                <div class="flex">browse all ads!</div>
+                            </a>
                         </div>
                     </div>
                 </div>
