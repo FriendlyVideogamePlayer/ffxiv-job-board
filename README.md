fbab3c5bc300fbe32ace01d8fed8d8505a784b36
FriendlyVideogamePlayer <zakariahahah@gmail.com>
2021-07-31 22:29:46 +0100

    Add a base homepage and update readme
    - simple clear glass container added

--- README.md
+++ README.md
@@ -1,3 +1,23 @@
 ## ffxiv job board
+This project is built using laravel and tailwind and so to install and use it you will need Composer installed on your server/PC.
+* [Laravel](https://laravel.com/) 
+* [Composer](https://getcomposer.org/) 
+* [Tailwind](https://tailwindcss.com/)
 
-project info
+Move this project folder to your webserver/XAMPP web root (XAMPP/htdocs or /var/www) and simply cd/to/project/directory and run 
+
+```
+composer install
+```
+
+Next to get the frontend dependencies, including tailwind run
+
+```
+npm install
+```
+
+Finally to generate the built frontend files run
+
+```
+npm run dev
+```
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,8 +5,25 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
         <title>Laravel</title>
         <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     </head>
-    <body class="antialiased">
-        
+    <body class="bg-gradient-to-r from-indigo-500 via-pink-500 to-yellow-500">
+        <div class="container mx-auto bg-cover min-h-screen w-full flex justify-center items-center">
+            <div class="w-full min-h-screen my-4 bg-white p-5 rounded-xl bg-opacity-40 backdrop-filter backdrop-blur-lg">
+                <div class="header-card justify-center flex font-semibold">
+                    <div class="text-center">
+                        <h1 class="text-xl pb-5 pt-5" role="heading" aria-level="1">ffxiv board </h1>
+                        <div class="index">
+                            <div class="mb-4">
+                                <a href="" class='justify-center flex w-44 bg-gradient-to-r from-indigo-500 via-pink-500 to-yellow-500 hover:from-indigo-600 hover:via-pink-600 hover:to-red-600 focus:outline-none text-white text-lg font-bold shadow-md rounded-full mx-auto p-2'>
+                                    <div class="flex">post an ad!</div>
+                                </a>
+                            </div>
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </body>
 </html>
