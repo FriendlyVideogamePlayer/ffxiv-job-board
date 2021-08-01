93aeaccb552143cee15783780e0b3a0f37b306d0
FriendlyVideogamePlayer <zakariahahah@gmail.com>
2021-08-01 12:21:28 +0100

    Create responsive navbar
    - added a navbar that's responsive

--- resources/views/postad.blade.php
+++ resources/views/postad.blade.php
@@ -2,9 +2,26 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     @include('components.head')
     <body class="bg-gradient-to-r from-indigo-500 via-pink-500 to-yellow-500">
-        <div class="w-full min-h-screen bg-white bg-opacity-20 backdrop-filter backdrop-blur-md">    
+        <div class="w-full min-h-screen bg-white bg-opacity-20 backdrop-filter backdrop-blur-md">
+            <div class="border-b-2 border-white w-full"> 
+                @include('components.nav')
+            </div> 
             <div class="container mx-auto bg-cover min-h-screen w-full flex justify-center items-center">
-            </div>
+                @include('components.filter')
+            <div>
+            <a href="#" class="inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-teal-500 hover:bg-white mt-4 lg:mt-0">Download</a>
         </div>
+
+
+        <script type="text/javascript">
+            function navbarMobile() {
+                var nav = document.getElementById("navbarMobile");
+                if (nav.className === "w-full hidden flex-grow lg:flex lg:items-center lg:w-auto") {
+                    nav.className = "w-full block flex-grow lg:flex lg:items-center lg:w-auto";
+                } else {
+                    nav.className = "w-full hidden flex-grow lg:flex lg:items-center lg:w-auto";
+                }
+            }
+        </script>
     </body>
 </html>
