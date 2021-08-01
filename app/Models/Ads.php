4b856d1aa290952dc61fb180e5ba615888e097f1
FriendlyVideogamePlayer <zakariahahah@gmail.com>
2021-08-01 16:24:16 +0100

    Create ad model and ad seeder
    - now the db is seeded with 10 example posts

---
+++ app/Models/Ads.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Ads extends Model
+{
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    public $fillable = [
+        'ad_id',
+        'title',
+        'description',
+        'discord_contact',
+        'ad_type',
+        'ad_posted',
+    ];
+}
---
+++ database/seeders/AdsSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class AdsSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // seeds the db with 10 example ads
+        for ($i=0; $i < 12; $i++) {
+            DB::table('ads')->insert([
+                'title' => 'lf 1x healers 2x tank 3x dps',
+                'description' => 'we are a really cool group and i need to make this example text quite long so i will continue to type. we are wanting to kill tea and you need to be the best player in the world in order to join us because we are also the best players in the world. please contact me so we can all be really good together.',
+                'discord_contact' => 'example#0000',
+                'ad_type' => 'static_rec',
+            ]);
+        }
+    }
+}
